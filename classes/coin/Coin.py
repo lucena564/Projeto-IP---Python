f372de34ec3e3006d2d8ce2da879ca2a8b02b5d8
Stela <stelaoliver2020@gmaiil.com>
2022-10-27 00:07:28 -0300

    Adding some .png's of collectables

--- classes/coin/Coin.py
+++ classes/coin/Coin.py
@@ -7,7 +7,7 @@ tile_size = BackgroundConstants.TILE_SIZE
 class Coin(pygame.sprite.Sprite):
     def __init__(self, x, y): 
         pygame.sprite.Sprite.__init__(self)
-        img = pygame.image.load(path.join('assets','characters','collectables','coin.png'))
+        img = pygame.image.load(path.join('assets','characters','collectables','milk-icon.png'))
         self.image = pygame.transform.scale(img, (tile_size // 2, tile_size // 2))
         self.rect = self.image.get_rect()
         self.rect.center = (x, y)
