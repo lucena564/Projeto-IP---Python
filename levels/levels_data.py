45215c2761ec06bf1e6c203978f2546bc1540027
Isaiaspereiraes <isaiaspereiraes@gmail.com>
2022-11-05 03:07:56 -0300

    level 8 added

--- levels/levels_data.py
+++ levels/levels_data.py
@@ -201,15 +201,16 @@ gameroom_img = pygame.image.load(
 garden_img = pygame.image.load(path.join('assets', 'background', 'garden.jpg'))
 # home_img = pygame.image.load(path.join('assets', 'background','home.jpg'))
 prison_img = pygame.image.load(path.join('assets', 'background', 'prison.jpg'))
-restaurant_img = pygame.image.load(
-    path.join('assets', 'background', 'restaurant.jpg'))
+restaurant_img = pygame.image.load(path.join('assets', 'background', 'restaurant.jpg'))
 
 
 # next_level_array = [(LEVEL_1_MAP_DIMENSIONS, bg_img, ''), (LEVEL_2_MAP_DIMENSIONS,
 #                                                            lib_img, ''), (LEVEL_3_MAP_DIMENSIONS, alien_test, 'alien')]
 
-next_level_array = [(LEVEL_1_MAP_DIMENSIONS, bg_img, ''), (LEVEL_2_MAP_DIMENSIONS, candy_img, 'candy'), (LEVEL_3_MAP_DIMENSIONS, gameroom_img, 'gameroom'), (LEVEL_5_MAP_DIMENSIONS,
-                                                                                                                                                             booksroom_img, 'booksroom'), (LEVEL_6_MAP_DIMENSIONS, prison_img, 'prison'), (LEVEL_4_MAP_DIMENSIONS, alien_test, 'alien'), (LEVEL_7_MAP_DIMENSIONS, garden_img, 'garden')]
+next_level_array = [(LEVEL_1_MAP_DIMENSIONS, bg_img, ''), (LEVEL_2_MAP_DIMENSIONS, candy_img, 'candy')
+                    , (LEVEL_3_MAP_DIMENSIONS, gameroom_img, 'gameroom'), (LEVEL_5_MAP_DIMENSIONS,booksroom_img, 'booksroom')
+                    , (LEVEL_6_MAP_DIMENSIONS, prison_img, 'prison'), (LEVEL_4_MAP_DIMENSIONS, alien_test, 'alien')
+                    , (LEVEL_7_MAP_DIMENSIONS, garden_img, 'garden'), (LEVEL_8_MAP_DIMENSIONS, garden_img, 'garden')]
 
 
 # next_level_array = [LEVEL_1_MAP_DIMENSIONS, LEVEL_2_MAP_DIMENSIONS, LEVEL_3_MAP_DIMENSIONS, LEVEL_4_MAP_DIMENSIONS, LEVEL_5_MAP_DIMENSIONS]
