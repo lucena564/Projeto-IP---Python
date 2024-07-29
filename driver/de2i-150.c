787149f603a001541d97046776e115e9200f4f78
AndreVVoigt <114924598+AndreVVoigt@users.noreply.github.com>
2024-07-29 14:40:50 -0300

    Update de2i-150.c
    Endereços atualizados

--- driver/de2i-150.c
+++ driver/de2i-150.c
@@ -8,7 +8,7 @@
 #include <linux/uaccess.h>	/* copy_*_user functions */
 #include <linux/pci.h>		/* pci funcs and types */
 
-#include "../include/ioctl_cmds.h"
+#include "../../include/ioctl_cmds.h"
 
 /* meta information */
 
@@ -125,7 +125,7 @@ static int __init my_init(void)
 	printk("my_driver: device number %d was registered!\n", MAJOR(my_device_nbr));
 
 	/* 2. create class : appears at /sys/class */
-	if ((my_class = class_create(THIS_MODULE, DRIVER_CLASS)) == NULL) {
+	if ((my_class = class_create(DRIVER_CLASS)) == NULL) {
 		printk("my_driver: device class count not be created!\n");
 		goto ClassError;
 	}
@@ -233,27 +233,27 @@ static long int my_ioctl(struct file*, unsigned int cmd, unsigned long arg)
 {
 	switch(cmd){
 	case RD_SWITCHES:
-		read_pointer = bar0_mmio + 0xC020; //TODO: update offset
+		read_pointer = bar0_mmio + 0xC080; //TODO: update offset
 		rd_name_idx = IDX_SWITCH;
 		break;
 	case RD_PBUTTONS:
-		read_pointer = bar0_mmio + 0xC060; //TODO: update offset
+		read_pointer = bar0_mmio + 0xC0A0; //TODO: update offset
 		rd_name_idx = IDX_PBUTTONS;
 		break;
 	case WR_L_DISPLAY:
-		write_pointer = bar0_mmio + 0xC040; //TODO: update offset
+		write_pointer = bar0_mmio + 0xC160;//TODO: update offset
 		wr_name_idx = IDX_DISPLAYL;
 		break;
 	case WR_R_DISPLAY:
-		write_pointer = bar0_mmio + 0xC000; //TODO: update offset
+		write_pointer = bar0_mmio + 0xC040;//TODO: update offset
 		wr_name_idx = IDX_DISPLAYR;
 		break;
 	case WR_RED_LEDS:
-		write_pointer = bar0_mmio + 0xC0A0; //TODO: update offset
+		write_pointer = bar0_mmio + 0xC210;//TODO: update offset
 		wr_name_idx = IDX_DISPLAYR;
 		break;
 	case WR_GREEN_LEDS:
-		write_pointer = bar0_mmio + 0xC080; //TODO: update offset
+		write_pointer = bar0_mmio + 0xC100;//TODO: update offset
 		wr_name_idx = IDX_DISPLAYR;
 		break;
 	default:
@@ -299,7 +299,7 @@ static int __init my_pci_probe(struct pci_dev *dev, const struct pci_device_id *
 
 	/* initialize a default peripheral read and write pointer */
 	write_pointer = bar0_mmio + 0xC000; //TODO: update offset
-	read_pointer  = bar0_mmio + 0xC060; //TODO: update offset
+	read_pointer  = bar0_mmio + 0xC080; //TODO: update offset
 
 	return 0;
 }
